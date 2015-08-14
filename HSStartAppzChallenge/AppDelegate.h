54ea2b23a9144c93f65b93a5088eac0638dd1086
HalahSalih <hala.raad.salih@gmail.com>
2015-08-14 17:19:55 +0300

    intial commit

---
+++ HSStartAppzChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HSStartAppzChallenge
+//
+//  Created by hala on 8/14/15.
+//  Copyright (c) 2015 Halah Salih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HSStartAppzChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HSStartAppzChallenge
+//
+//  Created by hala on 8/14/15.
+//  Copyright (c) 2015 Halah Salih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
