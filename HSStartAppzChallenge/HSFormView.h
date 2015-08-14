62db9b4b31c1c9df67a3951bc98a514154361a8d
HalahSalih <hala.raad.salih@gmail.com>
2015-08-14 21:05:05 +0300

    added form view

---
+++ HSStartAppzChallenge/HSFormView.h
@@ -0,0 +1,18 @@
+//
+//  HSFormView.h
+//  HSStartAppzChallenge
+//
+//  Created by hala on 8/14/15.
+//  Copyright (c) 2015 Halah Salih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSFormView : UIView
+
+@property (nonatomic, readonly) UITextField *usernameTextField;
+@property (nonatomic, readonly) UITextField *passwordTextField;
+
+@property (nonatomic, readonly) UIButton *submitButton;
+
+@end
---
+++ HSStartAppzChallenge/HSFormView.m
@@ -0,0 +1,72 @@
+//
+//  HSFormView.m
+//  HSStartAppzChallenge
+//
+//  Created by hala on 8/14/15.
+//  Copyright (c) 2015 Halah Salih. All rights reserved.
+//
+
+#import "HSFormView.h"
+
+const CGFloat X_PAADING = 15;
+const CGFloat Y_PAADING = 15;
+
+const CGFloat TEXTFIELD_HEIGHT = 30;
+
+
+@interface HSFormView ()
+
+@property (nonatomic, readwrite) UITextField *usernameTextField;
+@property (nonatomic, readwrite) UITextField *passwordTextField;
+
+@property (nonatomic, readwrite) UIButton *submitButton;
+
+@end
+@implementation HSFormView
+
+-(instancetype) init{
+    self = [super init];
+    
+    if(self){
+    
+        [self configureUserNameTextField];
+        [self configurePasswordTextField];
+        [self configureSubmitButton];
+    }
+    
+    return self;
+}
+
+-(void) layoutSubviews{
+    [super layoutSubviews];
+    CGFloat textFieldWidth = CGRectGetWidth(self.frame) - (2 * X_PAADING);
+    
+    self.usernameTextField.frame = CGRectMake(X_PAADING, Y_PAADING, textFieldWidth , TEXTFIELD_HEIGHT);
+    self.passwordTextField.frame = CGRectMake(X_PAADING, CGRectGetMaxY(self.usernameTextField.frame) + Y_PAADING, textFieldWidth , TEXTFIELD_HEIGHT);
+    
+    self.submitButton.frame = CGRectMake(X_PAADING, CGRectGetMaxY(self.passwordTextField.frame) + 2 *Y_PAADING, textFieldWidth, TEXTFIELD_HEIGHT);
+
+    
+}
+
+-(void) configureUserNameTextField{
+    
+    self.usernameTextField = [[UITextField alloc] init];
+    [self addSubview:self.usernameTextField];
+}
+
+-(void) configurePasswordTextField{
+    
+    self.passwordTextField = [[UITextField alloc] init];
+    self.passwordTextField.secureTextEntry = YES;
+    
+    [self addSubview:self.passwordTextField];
+}
+
+-(void) configureSubmitButton{
+    
+    self.submitButton = [[UIButton alloc] init];
+    [self addSubview:self.submitButton];
+}
+
+@end
